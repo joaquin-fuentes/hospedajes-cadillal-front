40e887d349cb5984729d183b192582374ad74cb9
Joaquin Fuentes <joaquin.fuentes@rollingcodestudio.com>
2025-01-05 21:37:51 -0300

    cambie importacion de navbar

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Esto está bien
-  css: {
-    preprocessorOptions: {
-      css: {
-        additionalData: `@import "slick-carousel/slick/slick.css";`,
-      },
-    },
+  base: "/", // Esto es importante para que no se rompan las rutas en producción
+  build: {
+    outDir: "dist", // Vercel espera el `dist/` por defecto
   },
 });
