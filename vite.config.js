2c6061e3b31e2bb7c48413b5f4c4805ece574a4c
Joaquin Fuentes <joaquin.fuentes@rollingcodestudio.com>
2025-01-05 21:35:33 -0300

    cambie importacion de navbar

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "_redirects", // Archivo de redirección
-          dest: ".", // Destino en la carpeta `dist/`
-        },
-      ],
-    }),
-  ],
-  base: "/",
+  plugins: [react()],
+  base: "/", // Esto está bien
   css: {
     preprocessorOptions: {
       css: {
