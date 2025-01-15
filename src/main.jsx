324d8105fb40a4d2f73558969d20eced0b147dec
Joaquin Fuentes <joaquin.fuentes@rollingcodestudio.com>
2025-01-15 12:26:21 -0300

    agregue analytics de vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from "./App.jsx";
 import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
